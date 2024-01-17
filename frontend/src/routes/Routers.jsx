1885c9db22d49f531f10a72a242b2fdd84169c6d
janvi jindal <janvijindal05@gmail.com>
2024-01-17 15:38:13 +0530

    first commit

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,36 @@
+// all pages
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Login from '../pages/Login'
+import Services from '../pages/Services'
+import Signup from '../pages/Signup'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
+import MyAccount from '../Dashboard/user-account/MyAccount'
+import Dashboard from '../Dashboard/doctor-account/Dashboard'
+
+import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute'
+
+const Routers = () => {
+  return <Routes>
+    <Route path="/" element={<Home/>}  />
+    <Route path="/home" element={<Home/>} />
+    <Route path="/doctors" element={<Doctors/>} />
+    <Route path="/doctors/:id" element={<DoctorsDetails/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/register" element={<Signup/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/services" element={<Services/>} />
+    <Route path="/users/profile/me" element={ <ProtectedRoute allowedRoles={['patient']}>
+                                                <MyAccount /> 
+                                              </ProtectedRoute>
+                                            } />
+    <Route path="/doctors/profile/me" element={ <ProtectedRoute allowedRoles={['doctor']}>
+                                                  <Dashboard />
+                                                </ProtectedRoute>
+                                              } />
+  </Routes>
+}
+
+export default Routers
